75662b28f391e6bbfbbc48fdeffce30fbc643b95
hanri2100 <162664555+hanri2100@users.noreply.github.com>
2025-11-25 06:10:39 +0800

    Update CSS hack comment for padding adjustment

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# --- CSS HACK UNTUK MENGURANGI PADDING ATAS ---
+# --- CSS HACK UNTUK MENGURANGI ATAU MENAMBAHKAN PADDING DI TIAP SISI ---
 st.markdown("""
     <style>
         .block-container {
